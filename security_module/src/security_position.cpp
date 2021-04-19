8cab7830d2a3b9621e5c91f9b2057b494aff5766
MingshanHe <hemingshan_1999@163.com>
2021-04-19 22:17:58 +0800

    2021/4/19 commit

--- security_module/src/security_position.cpp
+++ security_module/src/security_position.cpp
@@ -6,14 +6,21 @@
 
 using std::placeholders::_1;
 
-class MinimalSubscriber : public rclcpp::Node
+class MinimalSubscriber :
+    public rclcpp::Node
 {
-  public:
+public:
     MinimalSubscriber()
     : Node("minimal_subscriber")
     {
-      subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+        subscription_ = this->create_subscription<std_msgs::msg::String>(
+            "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1)
+        );
+
+        //TODO: Change the Topic of Joint States
+        Joint_Subscription = this->create_subscription<sensor_msgs::msg::JointState>(
+            "joint_states", 10, std::bind(&MinimalSubscriber::callback1, this, _1)
+        );
     }
 
   private:
@@ -21,13 +28,18 @@ class MinimalSubscriber : public rclcpp::Node
     {
       RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
     }
+    void callback1(const sensor_msgs::msg::JointState::SharedPtr msg)
+    {
+        auto position = msg->position;
+    }
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr Joint_Subscription;
 };
 
 int main(int argc, char * argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<MinimalSubscriber>());
+    rclcpp::shutdown();
+    return 0;
 }
