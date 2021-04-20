e57ed2de10e6f7aa9c2c909d0585eaf7103db960
MingshanHe <hemingshan_1999@163.com>
2021-04-20 12:07:56 +0800

    2021/4/20 commit

--- eci/src/Eci_keyboard.cpp
+++ eci/src/Eci_keyboard.cpp
@@ -5,6 +5,7 @@
 
 int   main()
 {
-    Motor_Position motor_position;
+    DWORD Active_MotorID[12]  = {0x00,0x00,0x00,0x00,0x605,0x607,0x606,0x608,0x605,0x607,0x606,0x608};
+    Motor_Position motor_position(Active_MotorID);
     motor_position.PositionControl();
 }
