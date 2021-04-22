fb0dc8c96ef329a5729c65e4c56238c4177acf58
MingshanHe <hemingshan_1999@163.com>
2021-04-22 22:03:02 +0800

    2021/4/22 commit

---
+++ exoskeleton_control/src/Eci/Angle_Send.cpp
@@ -0,0 +1,44 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+* member function as a callback from the timer. */
+
+class MinimalPublisher : public rclcpp::Node
+{
+  public:
+    MinimalPublisher()
+    : Node("minimal_publisher"), count_(0)
+    {
+      publisher_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("Joint_State_Send", 10);
+      timer_ = this->create_wall_timer(
+      10ms, std::bind(&MinimalPublisher::timer_callback, this));
+    }
+
+  private:
+    void timer_callback()
+    {
+      auto message = std_msgs::msg::Float64MultiArray();
+      message.data = {10.0,10.0,10.0,10.0};
+      publisher_->publish(message);
+      printf("Published!");
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr publisher_;
+    size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
