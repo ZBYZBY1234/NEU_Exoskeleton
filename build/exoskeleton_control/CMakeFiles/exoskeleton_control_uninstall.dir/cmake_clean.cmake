277d7693ce41cc4498d710fee4c8e2adca696be6
MingshanHe <hemingshan_1999@163.com>
2021-04-26 17:56:25 +0800

    2021/4/26 commit

--- build/exoskeleton_control/CMakeFiles/exoskeleton_control_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/exoskeleton_control_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/exoskeleton_control_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
