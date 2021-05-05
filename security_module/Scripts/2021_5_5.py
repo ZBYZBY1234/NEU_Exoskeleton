e8bde06eb6991343d09dda3df8dcb7a0a2dc313b
MingshanHe <hemingshan_1999@163.com>
2021-05-05 22:18:03 +0800

    2021/5/5 commit

--- security_module/Scripts/2021_5_5.py
+++ security_module/Scripts/2021_5_5.py
@@ -41,17 +41,17 @@ for item in reader:
     R_C_A_Driver.append(round(float(item[27])-float(item[26]),2))
 
     i = i+0.1
-plt.plot(x,L_T_A_Expect,label="Left_Thigh_Angle_Expect")
-plt.plot(x,L_T_A_Feedbk,label="Left_Thigh_Angle_Feedbk")
-plt.plot(x,L_T_A_Driver,label="Left_Thigh_Angle_Driver")
+# plt.plot(x,L_T_A_Expect,label="Left_Thigh_Angle_Expect")
+# plt.plot(x,L_T_A_Feedbk,label="Left_Thigh_Angle_Feedbk")
+# plt.plot(x,L_T_A_Driver,label="Left_Thigh_Angle_Driver")
 
 # plt.plot(x,L_C_A_Expect,label="Left_Calf_Angle_Expect")
 # plt.plot(x,L_C_A_Feedbk,label="Left_Calf_Angle_Feedbk")
 # plt.plot(x,L_C_A_Driver,label="Left_Calf_Angle_Driver")
 
-# plt.plot(x,L_T_A_Expect,label="Left_Thigh_Angle_Expect")
-# plt.plot(x,L_T_A_Feedbk,label="Left_Thigh_Angle_Feedbk")
-# plt.plot(x,L_T_A_Driver,label="Left_Thigh_Angle_Driver")
+plt.plot(x,R_T_A_Expect,label="Right_Thigh_Angle_Expect")
+plt.plot(x,R_T_A_Feedbk,label="Right_Thigh_Angle_Feedbk")
+plt.plot(x,R_T_A_Driver,label="Right_Thigh_Angle_Driver")
 
 # plt.plot(x,R_C_A_Expect,label="Right_Calf_Angle_Expect")
 # plt.plot(x,R_C_A_Feedbk,label="Right_Calf_Angle_Feedbk")
