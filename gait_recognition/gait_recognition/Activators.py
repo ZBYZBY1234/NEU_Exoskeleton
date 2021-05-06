d62a7b4bfa2cc4c8287c4e1449da73c7984f1eb7
MingshanHe <hemingshan_1999@163.com>
2021-05-06 13:49:37 +0800

    2021/5/6 commit

---
+++ gait_recognition/gait_recognition/Activators.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+class Relu_Activator(object):
+
+    def forward(self, weighted_input):
+        return max(0, weighted_input)
+
+    def backward(self, output):
+        return 1 if output > 0 else 0
+
+class Identity_Activator(object):
+
+    def forward(self, weighted_input):
+        return weighted_input
+
+    def backward(self, output):
+        return 1
+
+class Sigmoid_Activator(object):
+
+    def forward(self, weighted_input):
+        return 1.0 / (1.0 + np.exp(-weighted_input))
+
+    def backward(self, output):
+        return output * (1-output)
+
+class Tanh_Activator(object):
+
+    def forward(self, weighted_input):
+        return 2.0 / (1.0 + np.exp(-2 * weighted_input)) - 1.0
+
+    def backward(self, output):
+        return 1 - output * output
