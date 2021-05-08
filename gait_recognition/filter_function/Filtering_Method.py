16a4f54416d3e055752ea587fa12c687a65b70f3
MingshanHe <hemingshan_1999@163.com>
2021-05-08 14:38:27 +0800

    2021/5/8 commit

--- gait_recognition/filter_function/Filtering_Method.py
+++ gait_recognition/filter_function/Filtering_Method.py
@@ -5,10 +5,13 @@
 #    * @Description: 常见滤波算法
 # */
 import numpy as np
+
 #* 限幅滤波算法 *#
 #* Limiting Filter *#
 #* Input: 上一个量 Old_Value，当前量 Value， 滤波幅值 Variation_Range
 #* Output: 滤波结果量
+#* 优点: 能够有效克服因为偶然因粗引起的脉冲干扰
+#* 缺点: 无法抑制周期性的干扰
 
 def Limmiting_Filtering(Old_Value, Value, Variation_Range):
     if (abs(Value - Old_Value)>Variation_Range):
@@ -20,6 +23,8 @@ def Limmiting_Filtering(Old_Value, Value, Variation_Range):
 #* Median Filter *#
 #* Input: 待滤波的数据 Value_List, 采样次数 Sample_Times
 #* Output: 滤波结果量
+#* 优点: 能够有效克服因偶然因素引起的波动干扰
+#* 缺点: 对快速变化的不适宜
 
 def Median_Filter(Value_List, Sample_Times):
 
