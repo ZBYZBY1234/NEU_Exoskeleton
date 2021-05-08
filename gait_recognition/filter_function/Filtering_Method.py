f911edd6fe7da3ca3e88c9ad7f1888fb96f7a8a1
MingshanHe <hemingshan_1999@163.com>
2021-05-08 14:36:18 +0800

    2021/5/8 commit

--- gait_recognition/filter_function/Filtering_Method.py
+++ gait_recognition/filter_function/Filtering_Method.py
@@ -2,11 +2,40 @@
 #    * @Date: 2021-05-08 13:21:58
 #    * @Last Modified by:   Beal.MS
 #    * @Last Modified time: 2021-05-08 13:21:58
-#    * @Description: 限幅滤波算法
+#    * @Description: 常见滤波算法
 # */
+import numpy as np
+#* 限幅滤波算法 *#
+#* Limiting Filter *#
+#* Input: 上一个量 Old_Value，当前量 Value， 滤波幅值 Variation_Range
+#* Output: 滤波结果量
+
 def Limmiting_Filtering(Old_Value, Value, Variation_Range):
     if (abs(Value - Old_Value)>Variation_Range):
         return Old_Value
     else:
         return Value
 
+#* 中位值滤波算法 *#
+#* Median Filter *#
+#* Input: 待滤波的数据 Value_List, 采样次数 Sample_Times
+#* Output: 滤波结果量
+
+def Median_Filter(Value_List, Sample_Times):
+
+    Filtered_Value = []
+
+    for i in range(int(len(Value_List)/Sample_Times)):
+        Select_Slide = Value_List[i*Sample_Times : (i+1)*Sample_Times]
+        Median_Slide = np.median(Select_Slide)
+        for j in range(Sample_Times):
+            Filtered_Value.append(Median_Slide)
+
+    # 当采样次数无法整除数据量长度时需要考虑剩下的部分
+    if(len(Value_List)%Sample_Times != 0):
+        Select_Slide = Value_List[(i+1)*Sample_Times:]
+        Median_Slide = np.median(Select_Slide)
+        for j in range(len(Value_List)%Sample_Times):
+            Filtered_Value.append(Median_Slide)
+
+    return Filtered_Value
