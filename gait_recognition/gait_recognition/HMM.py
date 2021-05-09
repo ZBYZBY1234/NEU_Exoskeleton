f33866af96041794b17798fb6b66a97705c013d9
MingshanHe <hemingshan_1999@163.com>
2021-05-09 15:41:20 +0800

    2021/5/9 commit

--- gait_recognition/gait_recognition/HMM.py
+++ gait_recognition/gait_recognition/HMM.py
@@ -1,17 +1,26 @@
-# Hidden Markov Model
-# mode: (*)Stay,(*)Walk,(*)Stand Up,(*)Squat down,(*)Run
-# class HMM
-# # Main Function
-if __name__ == "__main__":
-    # nn = Network(3,3,3)
-    # cases  = [np.array([1,0,0]),np.array([0,1,0]),np.array([0,0,1])]
-    # labels = [np.array([0,1,0]),np.array([0,0,1]),np.array([1,0,0])]
-    # nn.train(case,label)
-    nn = Network(10,10,3)
 
-    cases,labels = pre_labels_cases()
-    nn.train(cases,labels,limit=500,learn=0.1)
-    print(nn.input_weights)
-    print(nn.output_weights)
-    for i in range(len(cases)):
-        print(nn.predict(cases[i]),labels[i])
+# /*
+#  * @Author: MingshanHe
+#  * @Email: hemingshan@robotics.github.com
+#  * @Date: 2021-02-03 15:16:20
+#  * @Last Modified by:   MingshanHe
+#  * @Last Modified time: 2021-02-03 15:16:20
+#  * @Description: This code for Question2
+#  */
+import numpy as np
+class markov(object):
+    def __init__ (self):
+        self.p     = np.array([[0.6,0.4,0],[0.6,0.3,0.1],[0,0.2,0.8]])
+        self.pi    = np.array([1,0,0])
+        self.times = 0
+
+    def predict(self,iters):
+        for i in range(iters):
+            self.pi = self.pi.dot(self.p)
+            self.times = i+1
+
+if __name__ == "__main__":
+    markov = markov()
+    markov.predict(1000)
+    print("Iterations times: ",markov.times,";","The pi of that times: ",markov.pi)
+    print("Normal: %.5f%% \nTired:  %.5f%%\nFever:  %.5f%%"%(markov.pi[0]*100,markov.pi[1]*100,markov.pi[2]*100))
