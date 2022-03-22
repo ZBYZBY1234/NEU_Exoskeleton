6a14dc7880195550f3a8c016cb26724b2ff87c1a
ZBYZBY1234 <100835482+ZBYZBY1234@users.noreply.github.com>
2022-03-22 09:56:55 +0800

    Add files via upload

--- ros_controllers/impedance_RBF_controller/include/impedance_RBF_controller/rbf_compute.h
+++ ros_controllers/impedance_RBF_controller/include/impedance_RBF_controller/rbf_compute.h
@@ -51,7 +51,7 @@ namespace imp_rbf_controller
         0, 0, 0, 5, 0,
         0, 0, 0, 0, 5;
     T_M12=T_M11;T_M13=T_M11;T_M14=T_M11;T_M21=T_M11;T_M22=T_M11;T_M23=T_M11;T_M24=T_M11;
-    T_M31=T_M11;T_M32=T_M11;T_M33=T_M11;T_M34=T_M11;T_M41=T_M41;T_M42=T_M41;T_M43=T_M11;T_M44=T_M11;
+    T_M31=T_M11;T_M32=T_M11;T_M33=T_M11;T_M34=T_M11;T_M41=T_M11;T_M42=T_M11;T_M43=T_M11;T_M44=T_M11;
 
 
     // T_G1 << imp_rbf_param["T_G1"].as<Eigen::Matrix<double,5,5>>();
@@ -69,7 +69,7 @@ namespace imp_rbf_controller
         0, 0, 0, 10, 0,
         0, 0, 0, 0, 10;
     T_C12=T_C11;T_C13=T_C11;T_C14=T_C11;T_C21=T_C11;T_C22=T_C11;T_C23=T_C11;T_C24=T_C11;
-    T_C31=T_C11;T_C32=T_C11;T_C33=T_C11;T_C34=T_C11;T_C41=T_C41;T_C42=T_C41;T_C43=T_C11;T_C44=T_C11;
+    T_C31=T_C11;T_C32=T_C11;T_C33=T_C11;T_C34=T_C11;T_C41=T_C11;T_C42=T_C11;T_C43=T_C11;T_C44=T_C11;
 
 
     b = 10;
@@ -109,7 +109,7 @@ namespace imp_rbf_controller
         exp(-pow((q - c_M.block(0, 4, 3, 0)).norm(), a )/pow(b,a) );
 
     h_M12=h_M11;h_M13=h_M11;h_M14=h_M11;h_M21=h_M11;h_M22=h_M11;h_M23=h_M11;h_M24=h_M11;
-    h_M31=h_M11;h_M32=h_M11;h_M33=h_M11;h_M34=h_M11;h_M41=h_M41;h_M42=h_M41;h_M43=h_M11;h_M44=h_M11;
+    h_M31=h_M11;h_M32=h_M11;h_M33=h_M11;h_M34=h_M11;h_M41=h_M11;h_M42=h_M11;h_M43=h_M11;h_M44=h_M11;
 
     h_G1 << exp(-pow((q - c_G.block(0, 0, 3, 0)).norm(), a )/pow(b,a) ),
         exp(-pow((q - c_G.block(0, 1, 3, 0)).norm(), a )/pow(b,a) ),
@@ -125,7 +125,7 @@ namespace imp_rbf_controller
          exp(-pow((z - c_C.block(0, 3, 7, 0)).norm(), a )/pow(b,a) ),
          exp(-pow((z - c_C.block(0, 4, 7, 0)).norm(), a )/pow(b,a) );
     h_C12=h_C11;h_C13=h_C11;h_C14=h_C11;h_C21=h_C11;h_C22=h_C11;h_C23=h_C11;h_C24=h_C11;
-    h_C31=h_C11;h_C32=h_C11;h_C33=h_C11;h_C34=h_C11;h_C41=h_C41;h_C42=h_C41;h_C43=h_C11;h_C44=h_C11;
+    h_C31=h_C11;h_C32=h_C11;h_C33=h_C11;h_C34=h_C11;h_C41=h_C11;h_C42=h_C11;h_C43=h_C11;h_C44=h_C11;
    
     e = qd - q;
 
@@ -137,25 +137,25 @@ namespace imp_rbf_controller
     dd_qr = dd_qd + fai * d_e;
 
     W_M11 << T_M11(0, 0) * h_M11(0, 0) * dd_qr(0, 0) * r(0, 0),
-        T_M11(1, 1)* h_M11(1, 0)* dd_qr(0, 0)* r(2, 0),
-        T_M11(2, 2)* h_M11(2, 0)* dd_qr(0, 0)* r(2, 0),
-        T_M11(3, 3)* h_M11(3, 0)* dd_qr(0, 0)* r(2, 0),
-        T_M11(4, 4)* h_M11(4, 0)* dd_qr(0, 0)* r(2, 0);
+        T_M11(1, 1)* h_M11(1, 0)* dd_qr(0, 0)* r(0, 0),
+        T_M11(2, 2)* h_M11(2, 0)* dd_qr(0, 0)* r(0, 0),
+        T_M11(3, 3)* h_M11(3, 0)* dd_qr(0, 0)* r(0, 0),
+        T_M11(4, 4)* h_M11(4, 0)* dd_qr(0, 0)* r(0, 0);
     W_M12 << T_M12(0, 0) * h_M12(0, 0) * dd_qr(1, 0) * r(0, 0),
-        T_M12(1, 1)* h_M12(1, 0)* dd_qr(1, 0)* r(2, 0),
-        T_M12(2, 2)* h_M12(2, 0)* dd_qr(1, 0)* r(2, 0),
-        T_M12(3, 3)* h_M12(3, 0)* dd_qr(1, 0)* r(2, 0),
-        T_M12(4, 4)* h_M12(4, 0)* dd_qr(1, 0)* r(2, 0);
+        T_M12(1, 1)* h_M12(1, 0)* dd_qr(1, 0)* r(0, 0),
+        T_M12(2, 2)* h_M12(2, 0)* dd_qr(1, 0)* r(0, 0),
+        T_M12(3, 3)* h_M12(3, 0)* dd_qr(1, 0)* r(0, 0),
+        T_M12(4, 4)* h_M12(4, 0)* dd_qr(1, 0)* r(0, 0);
     W_M13 << T_M13(0, 0) * h_M13(0, 0) * dd_qr(2, 0) * r(0, 0),
-        T_M13(1, 1)* h_M13(1, 0)* dd_qr(2, 0)* r(2, 0),
-        T_M13(2, 2)* h_M13(2, 0)* dd_qr(2, 0)* r(2, 0),
-        T_M13(3, 3)* h_M13(3, 0)* dd_qr(2, 0)* r(2, 0),
-        T_M13(4, 4)* h_M13(4, 0)* dd_qr(2, 0)* r(2, 0);
+        T_M13(1, 1)* h_M13(1, 0)* dd_qr(2, 0)* r(0, 0),
+        T_M13(2, 2)* h_M13(2, 0)* dd_qr(2, 0)* r(0, 0),
+        T_M13(3, 3)* h_M13(3, 0)* dd_qr(2, 0)* r(0, 0),
+        T_M13(4, 4)* h_M13(4, 0)* dd_qr(2, 0)* r(0, 0);
     W_M14 << T_M14(0, 0) * h_M14(0, 0) * dd_qr(3, 0) * r(0, 0),
-        T_M14(1, 1)* h_M14(1, 0)* dd_qr(3, 0)* r(2, 0),
-        T_M14(2, 2)* h_M14(2, 0)* dd_qr(3, 0)* r(2, 0),
-        T_M14(3, 3)* h_M14(3, 0)* dd_qr(3, 0)* r(2, 0),
-        T_M14(4, 4)* h_M14(4, 0)* dd_qr(3, 0)* r(2, 0);
+        T_M14(1, 1)* h_M14(1, 0)* dd_qr(3, 0)* r(0, 0),
+        T_M14(2, 2)* h_M14(2, 0)* dd_qr(3, 0)* r(0, 0),
+        T_M14(3, 3)* h_M14(3, 0)* dd_qr(3, 0)* r(0, 0),
+        T_M14(4, 4)* h_M14(4, 0)* dd_qr(3, 0)* r(0, 0);
     W_M21 << T_M21(0, 0) * h_M21(0, 0) * dd_qr(0, 0) * r(1, 0),
         T_M21(1, 1)* h_M21(1, 0)* dd_qr(0, 0)* r(1, 0),
         T_M21(2, 2)* h_M21(2, 0)* dd_qr(0, 0)* r(1, 0),
